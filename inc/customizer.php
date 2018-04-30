587fea2ea1a9a18f31ef1ae094c59b718d731c4b
drewe0327 <aenriquez@wonkasoft.com>
2018-04-30 13:31:14 -0700

    worked on adding header and footer theme options

--- inc/customizer.php
+++ inc/customizer.php
@@ -25,6 +25,88 @@ function keepcalm_homebuyers_customize_register( $wp_customize ) {
 			'render_callback' => 'keepcalm_homebuyers_customize_partial_blogdescription',
 		) );
 	}
+	/**
+	 * Theme options 
+	 * @since 1.0.0 [Theme options for the homepage]
+	 */
+	$wp_customize->add_panel( 'ft_theme_options', array(
+		'priority'		=> 1,
+		'capability' 	=> 'edit_theme_options',
+		'theme_support' => '',
+		'title' 		=> __( 'Theme Options', 'keepcalm-homebuyers' ),
+		'description'	=> __( 'Theme Settings', 'keepcalm-homebuyers' )
+		 ) );
+	/**
+	 * Putting in header image
+	 * @since 1.0.0 [Adding in header image]
+	 */
+	$wp_customize->add_section( 'header_section', array(
+		'capability'	=> 'edit_theme_options',
+		'theme_support'	=> '',
+		'priority'		=> 11,
+		'title'			=> __( 'Header Section', 'keepcalm-homebuyers' ),
+		'description'	=> __( 'Header Section Options', ' keepcalm-homebuyers' ),
+		'panel'			=> 'ft_theme_options' 
+		) );
+	/**
+	 * Headaing image setting
+	 * @since 1.0.0 [Header image setings]
+	 */
+	$wp_customize->add_setting( 'header_image', array(
+		'defualt'	=> '',
+		'transport'	=> 'refresh'
+		) );
+	/**
+	 * Controller
+	 * @since 1.0.0 [add control]
+	 */
+	$wp_customize->add_control( new WP_Customize_Image_Control(
+		$wp_customize,
+		'header_image', 
+		array(
+			'label' 		=> __( 'Main Header Image', 'keepcalm-homebuyers' ), 
+			'section'		=> 'header_section', 
+			'setting'		=> 'header_image', 
+			'type'			=> 'image',
+			'description'	=> __( 'Chose Header Image', 'keepcalm-homebuyers' )
+		)
+	) );
+	/**
+	 * Footer Section
+	 * @since 1.0.0 [adding footer section]
+	 */
+	$wp_customize->add_section( 'footer_section', array(
+		'capability' 	=> 'edit_theme_options',
+		'theme_support'	=> '',
+		'priority'		=> 15,
+		'title' 		=> __( 'Footer Section' , 'keepcalm-homebuyers'),
+		'description'	=> __( 'Footer Section Options', 'keepcalm-homebuyers' ),
+		'panel'			=> 'ft_theme_options'
+	) );
+	/**
+	 * Footer settings
+	 * @since 1.0.0
+	 */
+	$wp_customize->add_setting( 'copyright', array(
+		'default'	=> '',
+		'transport'	=> 'refresh'
+	) );
+	/**
+	 * Footer Controller
+	 * @since 1.0.0
+	 */
+	$wp_customize->add_control( new WP_Customize_Control(
+		$wp_customize,
+		'copyright',
+		array(
+		    'label' 		=> __( 'Copyright Text', 'keepcalm-homebuyers' ), 
+			'section'		=> 'footer_section', 
+			'setting'		=> 'copyright', 
+			'type'			=> 'text',
+			'description'	=> __( 'Input Copyright Information', 'keepcalm-homebuyers' )
+		)
+	) );
+
 }
 add_action( 'customize_register', 'keepcalm_homebuyers_customize_register' );
 
