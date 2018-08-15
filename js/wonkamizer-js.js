425b29a6ed96cacc24bd7c5c91cb694494740013
llister15 <llister@wonkasoft.com>
2018-08-14 17:35:15 -0700

    updated form

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -31,7 +31,7 @@ var placeSearch, autocomplete;
         route: 'long_name', // Street Name only
         locality: 'long_name', // City Name
         administrative_area_level_1: 'short_name', // State
-        country: 'short_name', // Country
+        country: 'long_name', // Country
         postal_code: 'short_name' // Zip Code
       };
 
@@ -50,7 +50,6 @@ function initAutocomplete() {
   function fillInAddress() {
     // Get the place details from the autocomplete object.
     var place = autocomplete.getPlace();
-
     // Get each component of the address from the place details
     // and fill the corresponding field on the form.
     for (var i = 0; i < place.address_components.length; i++) {
@@ -60,6 +59,8 @@ function initAutocomplete() {
         document.getElementById(addressType).value = val;
       }
     }
+
+    document.getElementById('street_number').value = place.address_components[0].long_name + ' ' + place.address_components[1].long_name;
 }
 
 // Bias the autocomplete object to the user's geographical location,
